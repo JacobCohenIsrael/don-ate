9208d3fa0bca57267aa722666aff23f2bbe918dc
listguy <nitzanjl@gmail.com>
2023-10-16 11:23:33 +0300

    added highscore and button functioning

--- Assets/Scripts/GameOverScreen.cs
+++ Assets/Scripts/GameOverScreen.cs
@@ -1,11 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameOverScreen : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI scoreText;
+    [SerializeField] TextMeshProUGUI highScoreText;
+    [SerializeField] TextMeshProUGUI highScoreAlertText;
     [SerializeField] private Counter delivered;
 
     [SerializeField] private GameEvent gameOverEvent;
@@ -19,9 +20,32 @@ public class GameOverScreen : MonoBehaviour
     public void OnGameOver()
     {
         this.gameObject.SetActive(true);
-        scoreText.text = $"שימחת {delivered.Value} חיילות וחיילים!";
+
+        float highScore = PlayerPrefs.GetFloat("HighScore", 0);
+        float score = delivered.Value;
+
+        scoreText.text = $"שימחת {score} חיילות וחיילים!";
+
+        if(score > highScore)
+        {
+            PlayerPrefs.SetFloat("HighScore", score);
+            PlayerPrefs.Save();
+            highScore = score;
+            highScoreAlertText.gameObject.SetActive(true);
+        }
+
+        highScoreText.text = $"שיא: {highScore}!";
     }
 
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
     private void OnDestroy()
     {
         gameOverEvent.UnregisterListener(OnGameOver);
