ca24f0ca99fb9d764dd89f9c59c41fdda3246dfa
listguy <nitzanjl@gmail.com>
2023-10-17 14:57:01 +0300

    added instructions screen + play instructions on first play

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -6,23 +6,41 @@ using UnityEngine.UI;
 public class MenuManager : MonoBehaviour
 {
     [SerializeField] private Button startGameButton;
+    [SerializeField] private Button instructionsButton;
     [SerializeField] private Button quitGameButton;
+    private int isFirstPlay;
 
     private void Awake()
     {
         startGameButton.onClick.AddListener(OnStartGame);
         quitGameButton.onClick.AddListener(OnExitGame);
+        instructionsButton.onClick.AddListener(OnInstructions);
+        isFirstPlay = PlayerPrefs.GetInt("FirstPlay", 1);
     }
 
     private void OnDestroy()
     {
         startGameButton.onClick.RemoveListener(OnStartGame);
         quitGameButton.onClick.RemoveListener(OnExitGame);
+        instructionsButton.onClick.RemoveListener(OnInstructions);
+
     }
 
     private void OnStartGame()
     {
-        SceneManager.LoadScene("Game");
+        if (isFirstPlay == 1)
+        {
+            SceneManager.LoadScene("Instructions");
+        }
+        else
+        {
+            SceneManager.LoadScene("Game");
+        }
+    }
+
+    private void OnInstructions()
+    {
+        SceneManager.LoadScene("Instructions");
     }
 
     private void OnExitGame()
---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Tutorial : MonoBehaviour
+{
+    [SerializeField] GameObject[] Screens;
+
+    private int screenIndex = 0;
+    private int isFirstPlay;
+
+    private void Awake()
+    {
+        isFirstPlay = PlayerPrefs.GetInt("FirstPlay", 1);
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(screenIndex == Screens.Length - 1)
+            {
+                if(isFirstPlay == 1)
+                {
+                    PlayerPrefs.SetInt("FirstPlay", 0);
+                    StartGame();
+                    return;
+                }
+                MainMenu();
+                return;
+            }
+
+            Screens[screenIndex + 1].SetActive(true);
+            Screens[screenIndex].SetActive(false);
+            screenIndex++;
+        }
+    }
+
+    private void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    private void MainMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
